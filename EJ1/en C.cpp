98941768d273fe14e52f6bbce1d6da41c8c8d7cb
ZoeAcero <zar.amazona@gmail.com>
2023-12-14 12:19:48 +0100

    18

---
+++ EJ1/en C.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+
+//Estructura estudiante
+struct Estudiante {
+    char nombre[50];
+    int edad;
+    float promedio;
+    char** materias;
+    int numMaterias;
+};
+
