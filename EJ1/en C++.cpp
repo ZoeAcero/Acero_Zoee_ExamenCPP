7072440e700af85cb208abd68afa7bd83f966a9e
ZoeAcero <zar.amazona@gmail.com>
2023-12-14 11:57:21 +0100

    3

--- EJ1/en C++.cpp
+++ EJ1/en C++.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <cstring>
+#include <string>
 
 //estructura estudiante
 struct estudiante{
@@ -9,4 +11,16 @@ struct estudiante{
     std::vector<std::string> materias;
 };
 
+//funcion para imprimir los datos de un estudiante
 
+void mostrarEstudiante(const Estudiante& estudiante) {
+    std::cout << "Nombre: " << estudiante.nombre << std::endl;
+    std::cout << "Edad: " << estudiante.edad << std::endl;
+    std::cout << "Promedio: " << estudiante.promedio << std::endl;
+
+    //imprimir materias
+    std::cout << "Materias: ";
+    for (const auto& materia : estudiante.materias) {
+        std::cout << materia << std::endl;
+    }
+}
